9ad92ea0281c11011a57dea4c3ff5a3cc597b9cb
Jacob Reynolds <jacobreynolds1115@gmail.com>
2023-12-26 09:56:30 -0600

    first commit

---
+++ src/main/java/com/transcriptionProject/TranscriptionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.transcriptionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscriptionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscriptionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transcriptionProject/TranscriptionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transcriptionProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscriptionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
